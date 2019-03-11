8a0a701c6b1dccc14ee2159df63ce39efb74a1c1
doanHa <dmha@uh.edu>
2019-03-11 15:20:19 -0500

    Added two and person java class

---
+++ src/demo/Person.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Person {
+	String name;
+	int age;
+	boolean hasPassport;
+	char gender;
+	float salary;
+	boolean isWorking;
+}
---
+++ src/demo/Two.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Two {
+	public static void main(String[] args) {
+		int x = 60;
+		if(x < 60 & ++x >= 61) {
+			System.out.println(x);
+		}
+		System.out.println(x);
+	}
+}
